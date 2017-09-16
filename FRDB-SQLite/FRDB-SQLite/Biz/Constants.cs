be4513f992eb3e45505d599786f8f4f98bc439d2
xuan <kimxuanhuynh21@gmail.com>
2017-09-16 19:52:28 +0700

    WIP

---
+++ FRDB-SQLite/FRDB-SQLite/Biz/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FRDB_SQLite.Biz
+{
+    static public class Constants
+    {
+        static public class QUERY_TYPE
+        {
+            public const String SINGLE = "Constants/QUERY_TYPE/SINGLE";
+            public const String MUTIPLE = "Constants/QUERY_TYPE/MUTIPLE";
+        }
+    }
+}
---
+++ FRDB-SQLite/FRDB-SQLite/Biz/QueryExecutionBLLv2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FRDB_SQLite.Biz;
+
+namespace FRDB_SQLite.Biz
+{
+    public class QueryExecutionBLLv2
+    {
+        private String _queryText;
+        private String _queryType;
+        private FdbEntity _fdbEntity;
+        public QueryExecutionBLLv2(String queryText, FdbEntity fdbEntity)
+        {
+            this._queryText = queryText.ToLower();
+            this._fdbEntity = fdbEntity;
+
+            QueryAnalyze();
+        }
+
+        private void QueryAnalyze()
+        {
+            if (this._queryText.Contains("union"))
+            {
+                this._queryType = Constants.QUERY_TYPE.MUTIPLE;
+            }
+            if (this._queryType == Constants.QUERY_TYPE.MUTIPLE)
+            {
+                List<String> singleQueries = SplitQuery(this._queryType);
+            }
+        }
+
+        private List<String> SplitQuery(String mutipleQueryText)
+        {
+            List<String> singleQueries = new List<String>();
+            singleQueries = mutipleQueryText.Split(new String[] { "contains" }, StringSplitOptions.None).ToList();
+            return singleQueries;
+        }
+    }
+}
